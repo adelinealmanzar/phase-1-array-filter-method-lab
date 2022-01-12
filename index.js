77a59de2ffa4a61bc52764c42cd3eefa6eca8588
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-01-12 15:30:04 -0800

    commit tree c693747d065524d7a78b0ce2a4092ac575c9cdb3

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(array, name) {
+    return array.filter(el => el === name || el === name.toLowerCase());
+}
+
+function fuzzyMatch(array, letters) {
+    return array.filter(el => el.slice(0, -3) === letters);
+}
+
+function matchName(array, nameToTest) {
+    return array.filter(el => el.name === nameToTest)
+}
